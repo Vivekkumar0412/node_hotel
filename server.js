501057b1ef7b76dc367b8c75e6095c4bb2b1ef8b
Vivekkumar0412 <www.vivekkumar0412@gmail.com>
2024-03-20 22:28:36 +0530

    Minor changes

--- server.js
+++ server.js
@@ -1,11 +1,43 @@
 const express = require('express');
-require('dotenv').config();
-const db = require('./db')
-
-const bodyParser = require('body-parser');
 let app = express();
-app.use(bodyParser.json());
+require('dotenv').config();
+const db = require('./db');
+const passport = require('passport');
+const localStrategy = require('passport-local').Strategy;
+const person = require('./models/person');
 
+// making middleware
+function getLog(req,res,next){
+    console.log(`log time [${new Date().toLocaleDateString()}] and route is ${req.originalUrl}`);
+    next();
+}
+// creating authencatision
+
+passport.use(new localStrategy(async (username, password, done)=>{
+    try{
+        let user = await person.findOne({username : username});
+        if(!user){
+            return done(null,false,{msg: "wrong username"});
+        }
+        const isMatchedPassword = user.password === password ? true:false;
+        if(isMatchedPassword){
+            return done(null, user);
+        }else{
+            return done(null, false, {msg : "wrong password"});
+        }
+    }catch(err){
+        return done(err);
+    }
+}))
+
+// very important without this passport will not work
+app.use(passport.initialize());
+// consumig the getLog() middleware
+app.use(getLog);
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+const localAuthMiddleware = passport.authenticate('local',{session:false});
 app.get('/',(req,res)=>{
     res.send("hi there")
 })
@@ -18,5 +50,5 @@ app.listen(PORT,()=>{
 })
 const personRoute = require('./routes/personRoutes');
 const menuRoute = require('./routes/menuRoutes');
-app.use('/person',personRoute);
+app.use('/person',localAuthMiddleware,personRoute);
 app.use('/menu',menuRoute);
