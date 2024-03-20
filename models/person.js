59c7672c18ea4eca6dde4d0e94e255a1073a89a8
Vivekkumar0412 <www.vivekkumar0412@gmail.com>
2024-03-20 22:28:17 +0530

    Minor changes

--- models/person.js
+++ models/person.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 const personSchema = new mongoose.Schema({
     name : {
         type : String,
@@ -25,7 +26,14 @@ const personSchema = new mongoose.Schema({
     salary : {
         type : Number,
         required : true
+    },
+    username : {
+        type : String,
+        required : true
+    },
+    password : {
+        type : String,
+        required : true
     }
 });
-
 module.exports = mongoose.model('person',personSchema);
